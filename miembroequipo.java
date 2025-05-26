4af61ba9e96735e9f0e1bb73c42e2a988b4bf95b
kevinduran <kevinsanchezduran4@gmail.com>
2025-05-26 10:36:38 -0600

    clase miembro equipo

--- miembroequipo.java
+++ miembroequipo.java
@@ -1,3 +1,56 @@
-public class miembroequipo {
+public class miembroequipo { 
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private Integer edad;
 
+    // Constructor
+    public miembroequipo(Integer id, String nombre, String apellidos, Integer edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    // Getters y Setters (Encapsulación)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    // Métodos comunes
+    public void concentrarse() {
+        System.out.println("Concentrándose...");
+    }
+
+    public void viajar() {
+        System.out.println("Viajando...");
+    }
 }
